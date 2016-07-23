61eac155f83ca84a3a5047b076615564b4b5987c
shiva.ping@gmail.com <Moukhthi9>
2016-07-23 17:59:29 +0530

    SignUp and Login Pages.

---
+++ JobPortal.Web/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobPortal.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ViewSignUp()
+        {
+            return PartialView("_SignUp");
+        }
+        [HttpPost]
+        public ActionResult SignUp()
+        {
+            return RedirectToAction("Index");
+        }
+        public ActionResult ViewLogin()
+        {
+            return PartialView("_Login");
+        }
+        [HttpPost]
+        public ActionResult Login()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
